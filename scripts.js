5d253b7076c3dfad0f78b191e45e1e517b083cca
MichaelHaydenDuran <156373801+MichaelHaydenDuran@users.noreply.github.com>
2024-02-01 11:59:03 -0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var dogeModal = new bootstrap.Modal(document.getElementById('dogeModal'));
-    $("#doge").draggable();
-    $("#hole").droppable({
-        drop: function( event, ui ) {
-          dogeModal.show();
-        }
-    });
-});
