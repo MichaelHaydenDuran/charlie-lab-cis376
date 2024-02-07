d9e0d4cdeb1d6e60514888d33d8e8ae98f421136
MichaelHaydenDuran <156373801+MichaelHaydenDuran@users.noreply.github.com>
2024-02-06 22:17:22 -0600

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,21 @@
-$(document).ready(function(){
+$(function(){
     var dogeModal = new bootstrap.Modal(document.getElementById('dogeModal'));
-    $("#doge").draggable();
+    var dogePic = $("#doge");
+    dogePic.draggable();
+    document.getElementById('dogeModal').addEventListener('hide.bs.modal', function (event) {
+      dogePic.css('top', '0').css('left', '0').css("width","250px");
+    });
     $("#hole").droppable({
         drop: function( event, ui ) {
-          dogeModal.show();
+          dogePic.animate({
+            width: 0
+          },
+          {
+            duration: 750,
+            complete: function() {
+              dogeModal.show();
+            }
+          });
         }
     });
 });
